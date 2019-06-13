54bb9052f907190611a1972b707e5b49ea2d80a6
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-06-13 10:24:51 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,40 @@
+var array, n, num, i;
+
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i===1) {
+      array.push('I am 1 strange loop.');
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  while (n>0) {
+    console.log(--n);
+  }
+  return("done");
+}
+
+function incrementVariable (i) {
+  i = num - 1;
+  return i;
+}
+
+function doWhileLoop(num) {
+    do {
+      console.log("I run once regardless.");
+    } while (incrementVariable(i) < num);
+  }
+
+var array = [];
+var n= 5;
+var num = 10;
+
+forLoop (array);
+
+whileLoop(n);
+
+doWhileLoop()();
